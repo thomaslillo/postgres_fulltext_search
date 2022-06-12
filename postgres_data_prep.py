52aebc84dcbce6a3b598f107c3fe527b034cdb99
Thomas Lillo <thomas.j.lillo@gmail.com>
2022-06-12 16:11:07 -0400

    first commit of sudo code

---
+++ postgres_data_prep.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import csv
+import sqlalchemy
+
+
+# Create Dataframe
+
+# for all the text files in a folder
+
+# Import the Text File
+
+# create the company name and data columns from the name
+
+# read all the contents of the file into the full_text column
+
+# append the file to a dataframe
+
+# write the full dataframe to a csv - to go into postgress
