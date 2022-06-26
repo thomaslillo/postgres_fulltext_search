35342f5690afc7def65231f674526c1d33fe134e
thomaslilo <thomas.j.lilo@gmail.com>
2022-06-26 17:09:58 -0400

    finished csv to upload and table creation script

--- postgres_data_prep.py
+++ postgres_data_prep.py
@@ -1,42 +1,45 @@
-from nbformat import read
 import pandas as pd
-import csv
-import sqlalchemy
 import config
 from os import listdir
 from os.path import isfile, join
-import re
 
-class PostgresETL:
+class BaseFileCreation:
 
-    def init(self):
+    def __init__(self):
         # dir where the files sit
-        self.path = config.configs['sampledir']        
+        self.path = config.configs['sampledir']
+        self.dest = r'C:\Users\Neptune\Documents\WOVEN\NI43101'
 
-    def createDF(self):
+    def create_base_df(self):
         # get a list of all the files
         files = [[f, join(self.path,f)] for f in listdir(self.path) if isfile(join(self.path,f))]
 
         # Create Dataframe
-        self.df = pd.DataFrame(files, columns=['FileName','FilePath'])
+        self.df = pd.DataFrame(files, columns=['filename','_FilePath'])
 
         # parse out date and company into column
-        self.dfdf['Company'] = self.dfdf['FileName'].str.extract(r'^\d+.\s(.*)\s[A-Z]{1}[a-z]{2}\s\d+\s\d{4}.txt')
-        self.dfdf['Date'] = self.dfdf['FileName'].str.extract(r'^\d+.\s.*\s([A-Z]{1}[a-z]{2}\s\d+\s\d{4}).txt')
+        self.df['company'] = self.df['filename'].str.extract(r'^\d+.\s(.*)\s[A-Z]{1}[a-z]{2}\s\d+\s\d{4}.txt')
+        self.df['date'] = self.df['filename'].str.extract(r'^\d+.\s.*\s([A-Z]{1}[a-z]{2}\s\d+\s\d{4}).txt')
 
-    # for all the text files in a folder
-    # with open("",r)
+        print(self.df.head())
 
-    # Import the Text File
+        def add_text_column(path):
+            # Import the Text File
+            with open(path, encoding="utf-8") as f:
+                text = f.read()
+            return text
 
-
-    # create the company name and data columns from the name
-
-    # read all the contents of the file into the full_text column
-
-    # append the file to a dataframe
-
-    # write the full dataframe to a csv - to go into postgress
+        # create the text col with the contents of each file
+        self.df['fulltext'] = self.df['_FilePath'].astype(str).apply(add_text_column)
+        
+        # remove the path field
+        self.df = self.df[['filename','company','date','fulltext']]    
+    
+    def create_csv(self):
+        self.df.to_csv(join(self.dest,"to_upload.csv"), index=False, encoding='utf-8')
 
 if __name__ == '__main__':
-    pg = PostgresETL()
+    pg = BaseFileCreation()
+    pg.create_base_df()
+    print(pg.df.head())
+    pg.create_csv()
